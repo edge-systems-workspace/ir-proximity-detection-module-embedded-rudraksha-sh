247647adcf44baae217181005d6ee7817ddd6bc2
RUDRAKSHA SHARMA <karlecoding@gmail.com>
2026-02-20 23:25:58 +0530

    first commit

--- src/main.cpp
+++ src/main.cpp
@@ -2,43 +2,44 @@
 /**
  * @file main.cpp
  * @brief Embedded Obstacle Detection System using IR Sensor
- * @author YOUR_NAME
- * @date YYYY-MM-DD
+ * @author RUDRAKSHA SHARMA (Git- rudraksha-sh )
+ * @date 2026-01-29
  *
  * @details
  * Reads digital input from IR sensor
  * and detects obstacle presence.
  */
 
- // TODO 1:
- // Define IR sensor digital pin (Use pin 2)
+// TODO 1: Define IR sensor digital pin (Use pin 2)
+#define IRPIN 2
 
- // TODO 2:
- // Create variable to store sensor state
+// TODO 2: Create variable to store sensor state
+int irState = 0;
 
 void setup() {
 
-    // TODO 3:
-    // Initialize Serial communication (9600 baud rate)
+    // TODO 3: Initialize Serial communication (9600 baud rate)
+    Serial.begin(9600);
 
-    // TODO 4:
-    // Configure IR pin as INPUT
+    // TODO 4: Configure IR pin as INPUT
+    pinMode(IRPIN, INPUT);
 
-    // TODO 5:
-    // Print system initialization message
+    // TODO 5: Print system initialization message
+    Serial.println("IR Obstacle Detection System Initialized");
 }
 
 void loop() {
 
-    // TODO 6:
-    // Read digital value from IR sensor
+    // TODO 6: Read digital value from IR sensor
+    irState = digitalRead(IRPIN);
 
-    // TODO 7:
-    // If obstacle detected
-    //     Print "Obstacle Detected"
-    // Else
-    //     Print "No Obstacle"
+    // TODO 7: If obstacle detected
+    if (irState == HIGH) {
+        Serial.println("Obstacle Detected");
+    } else {
+        Serial.println("No Obstacle");
+    }
 
-    // TODO 8:
-    // Add small delay (200–500ms)
+    // TODO 8: Add small delay (200–500ms)
+    delay(300);
 }
